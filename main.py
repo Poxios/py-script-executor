e44da343e4a47bf557c1ef6367a1b64fb6d2a242
Poxios <poxios0310@gmail.com>
2024-07-19 17:17:09 +0900

    updated

--- main.py
+++ main.py
@@ -8,18 +8,26 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/code-push', methods=['POST'])
+
+@app.route("/code-push", methods=["POST"])
 def make_pull():
     data = request.get_json()
-    env_password = os.getenv('PASSWORD')
-    if data and 'password' in data and data['password'] == env_password:
+    env_password = os.getenv("PASSWORD")
+    if data and "password" in data and data["password"] == env_password:
         try:
-            result = subprocess.run(['./script.sh'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            return jsonify({'status': 'success'})
+            result = subprocess.run(
+                ["./script.sh"],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            print(result)
+            return jsonify({"status": "success"})
         except subprocess.CalledProcessError as e:
-            return jsonify({'status': 'error'}), 500
+            return jsonify({"status": "error"}), 500
     else:
-        return jsonify({'status': 'error', 'message': 'Invalid password'}), 403
+        return jsonify({"status": "error", "message": "Invalid password"}), 403
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
